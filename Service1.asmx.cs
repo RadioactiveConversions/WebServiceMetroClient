82c5d374027af9a1236bb08f43f245b6f649b54b
kerenskylegacy <kerensky.legacy@gmail.com>
2014-10-01 13:24:29 +0200

    Beta 0.1
    sln und suo in das Folder davor verschieben

--- Service1.asmx.cs
+++ Service1.asmx.cs
@@ -17,10 +17,77 @@ namespace WebServiceMetroClient
     public class Service1 : System.Web.Services.WebService
     {
 
+        //Parametererklärung:
+        //  ID          = Charakter/Benutzer-ID
+        //  count       = Anzahl
+        //  isVerified  = ob der Nutzer Daten zu seinem eigenem Char abruft oder einem fremden
+        //  item        = Itemname
+        //  ph          = physischer Schaden
+        //  st          = geistiger Schaden
+
+
         [WebMethod]
-        public string HelloWorld()
+        //checkt ob der User vorhanden ist
+        public string verifyUser(int ID)
         {
-            return "Hello World";
+            return "";
+        }
+
+        [WebMethod]
+        //ruft die letzte Änderung ab
+        public string getLastChange()
+        {
+            return "";
+        }
+
+        [WebMethod]
+        //ruft den Charakter ab
+        public string getCharacter(int ID)
+        {
+            return "";
+        }
+
+        [WebMethod]
+        //ruft das Inventar des Charakters ab
+        public string getInventory(int ID, bool isVerified)
+        {
+            return "";
+        }
+
+        [WebMethod]
+        //setzt den Schadensmonitor des Charakters
+        public bool setDamage(int ID, int ph, int st)
+        {
+            return true;
+        }
+
+        [WebMethod]
+        //ändert Anzahl der Items
+        public bool alterItemAmount(int ID, string Item, int count)
+        {
+            if (count == 0)
+            {
+                //entferne Eintrag
+            }
+            else
+            {
+                //ändere Anzahl
+            }
+            return true;
+        }
+
+        [WebMethod]
+        //fügt ein NEUES Item in bestimmer Anzahl hinzu
+        public bool addItemAmount(int ID, string Item, int count)
+        {
+            return true;
+        }
+
+        [WebMethod]
+        //ändert die Munitionszahl, alle Typen sollen im schon in der Datenbank vorhanden sein und auf 0 gesetzt sein
+        public bool setAmmoAmount(int ID, string Item, int count)
+        {
+            return true;
         }
     }
 }
